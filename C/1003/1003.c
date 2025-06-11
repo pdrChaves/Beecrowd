b5ef4bf71c23f720e38973f186fdfbf22402c5e0
Pedro <pedroa.chaves10@gmail.com>
2025-06-11 16:54:54 -0300

    Prep maratona

---
+++ C/1003/1003.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (){
+    int A,B,Soma;
+    scanf("%d",&A);
+    scanf("%d",&B);
+    Soma = A + B;
+    printf("SOMA = %d\n",Soma);    
+    return 0;
+}
---
+++ C/1004/1004.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (){
+    int A, B, Prod;
+    scanf("%d",&A);
+    scanf("%d",&B);
+    Prod = A * B;
+    printf("PROD = %d\n", Prod);
+return 0;
+}
