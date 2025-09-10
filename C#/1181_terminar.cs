19407b1f229425a8dddf839e5c24ad5738a47962
Pedro Chaves <pedroa.chaves10@gmail.com>
2025-09-10 09:30:04 -0300

    Create 1181_terminar.cs

---
+++ C#/1181_terminar.cs
@@ -0,0 +1,30 @@
+using System;
+class URI
+{
+    public static void Main(string[] args)
+    {
+        double[,] M = new double[12, 12];
+        double L = double.Parse(Console.ReadLine());
+        int linha;
+        string op = Console.ReadLine();
+        linha = int.Parse(Console.ReadLine());
+        for (int i = 0; i < 12; i++)
+        {
+            if (op == "S") 
+            {
+                double somatoria = M[i,];
+            }
+            else if (op == "M")
+            {
+            }
+
+        }
+        for (int i = linha; i < 12; i++)
+        {
+            for (int j = 0; j < 12; j++)
+            {
+            }
+        }
+
+    }
+}
